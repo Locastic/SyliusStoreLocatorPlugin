5ad6ce3ca0f889c41a626621d46217a1201fcd6a
Antonio Peric-Mazar <antonio@locastic.com>
2018-06-06 19:02:44 +0200

    added translation for store

---
+++ src/Form/Type/StoreType.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Locastic\SyliusStoreLocatorPlugin\Form\Type;
+
+use Locastic\SyliusStoreLocatorPlugin\Form\Type\Translation\StoreTranslationType;
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Sylius\Bundle\ResourceBundle\Form\Type\ResourceTranslationsType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class StoreType extends AbstractResourceType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('code', TextType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.code',
+                'disabled' => null !== $builder->getData()->getCode(),
+            ])
+            ->add('enabled', CheckboxType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.enabled',
+            ])
+            ->add('latitude', NumberType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.latitude',
+            ])
+            ->add('longitude', NumberType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.longitude',
+            ])
+            ->add('contactPhone', TextType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.contact_phone',
+            ])
+            ->add('contactEmail', EmailType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.contact_email',
+            ])
+            ->add('translations', ResourceTranslationsType::class, [
+                'label' => 'bitbag_sylius_cms_plugin.ui.images',
+                'entry_type' => StoreTranslationType::class,
+            ])
+        ;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'locastic_sylius_store_locator_plugin_page';
+    }
+}
---
+++ src/Form/Type/Translation/StoreTranslationType.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Locastic\SyliusStoreLocatorPlugin\Form\Type\Translation;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class StoreTranslationType extends AbstractResourceType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.name',
+            ])
+            ->add('slug', TextType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.slug',
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.content',
+            ])
+            ->add('metaTitle', TextareaType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.meta_title',
+                'required' => false,
+            ])
+            ->add('metaKeywords', TextareaType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.meta_keywords',
+                'required' => false,
+            ])
+            ->add('metaDescription', TextareaType::class, [
+                'label' => 'locastic_sylius_store_locator_plugin.ui.meta_description',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'locastic_sylius_store_locator_plugin_store_translation';
+    }
+}
