c3aec58b977068ba723ac28d942e1111dd92530b
Antonio Peric-Mazar <antonio@locastic.com>
2018-06-07 11:05:59 +0200

    code cleanup

--- src/DependencyInjection/LocasticSyliusStoreLocatorPluginExtension.php
+++ src/DependencyInjection/LocasticSyliusStoreLocatorPluginExtension.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 namespace Locastic\SyliusStoreLocatorPlugin\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class LocasticSyliusStoreLocatorPluginExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $config, ContainerBuilder $container): void
     {
-        $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
     }
 }
