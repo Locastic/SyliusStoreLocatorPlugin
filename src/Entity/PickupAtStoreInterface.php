b19867eb205c83a0a4082ddb365d25d4553e79a0
Antonio Peric-Mazar <antonio@locastic.com>
2018-06-10 23:40:15 +0200

    init pickupAtStore option

---
+++ src/Entity/PickupAtStoreInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Locastic\SyliusStoreLocatorPlugin\Entity;
+
+
+interface PickupAtStoreInterface
+{
+    public function getStore(): ?StoreInterface;
+
+    public function setStore(?StoreInterface $store): void;
+}
---
+++ src/Form/Extension/Checkout/ShipmentTypeExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Locastic\SyliusStoreLocatorPlugin\Form\Extension\Checkout;
+
+
+use Locastic\SyliusStoreLocatorPlugin\Entity\Store;
+use Sylius\Bundle\CoreBundle\Form\Type\Checkout\ShipmentType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ShipmentTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        dump($options);
+
+        // todo find some way to get info about shipmening methods
+        // if methos is supporting pickupAtLocation add store fiedl
+        // we nned to add bool to ShippingMethod (isPickupAtLocation)
+        // this should be mapped
+
+        $builder->add('store', EntityType::class, [
+            'mapped' => false,
+            'class' => Store::class,
+            'required' => false,
+            'choice_label' => 'name'
+        ]);
+
+    }
+
+    public function getExtendedType(): string
+    {
+        return ShipmentType::class;
+    }
+}
