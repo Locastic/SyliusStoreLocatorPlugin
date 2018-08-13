1f58e3635eaf592c4e6025180665361b92f1cef8
Duje Šarić <duje.saric@locastic.com>
2018-08-13 11:53:00 +0200

    Fixed bugs with wrong checking is pickup address selected and fixed js bug with wrong rendering of pickup field.

--- src/Validator/Constraints/IsStorePopulatedValidator.php
+++ src/Validator/Constraints/IsStorePopulatedValidator.php
@@ -9,7 +9,7 @@ class IsStorePopulatedValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
     {
-        if ($value->getMethod()->isPickupAtLocation() && $value->getStore() === null) {
+        if ($value->getMethod()->isPickupAtStore() && $value->getStore() === null) {
             $this->context
                 ->buildViolation($constraint->message)
                 ->atPath('method')
