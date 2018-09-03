b9b5fe472f508f4d86a68359dc0bb49e49f23b4b
Duje Šarić <duje.saric@locastic.com>
2018-09-03 16:28:09 +0200

    Refactor => removed unused shipping method factory fields.

--- src/Fixture/ShippingMethodFactory.php
+++ src/Fixture/ShippingMethodFactory.php
@@ -6,56 +6,19 @@ namespace Locastic\SyliusStoreLocatorPlugin\Fixture;
 
 use Locastic\SyliusStoreLocatorPlugin\Entity\ShippingMethodInterface;
 use Sylius\Bundle\CoreBundle\Fixture\Factory\ShippingMethodExampleFactory;
-use Sylius\Bundle\CoreBundle\Fixture\OptionsResolver\LazyOption;
-use Sylius\Component\Addressing\Model\ZoneInterface;
 use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
-use Sylius\Component\Core\Formatter\StringInflector;
-use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Core\Model\ShippingMethodInterface as BaseShippingMethodInterface;
-use Sylius\Component\Locale\Model\LocaleInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
-use Sylius\Component\Shipping\Calculator\DefaultCalculators;
-use Sylius\Component\Shipping\Model\ShippingCategoryInterface;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ShippingMethodFactory extends ShippingMethodExampleFactory
 {
     /**
-     * @var FactoryInterface
-     */
-    private $shippingMethodFactory;
-
-    /**
-     * @var RepositoryInterface
-     */
-    private $zoneRepository;
-
-    /**
-     * @var RepositoryInterface
-     */
-    private $shippingCategoryRepository;
-
-    /**
-     * @var RepositoryInterface
-     */
-    private $localeRepository;
-
-    /**
-     * @var ChannelRepositoryInterface
-     */
-    private $channelRepository;
-
-    /**
      * @var \Faker\Generator
      */
     private $faker;
-
-    /**
-     * @var OptionsResolver
-     */
-    private $optionsResolver;
 
     /**
      * @param FactoryInterface $shippingMethodFactory
@@ -75,7 +38,6 @@ class ShippingMethodFactory extends ShippingMethodExampleFactory
     }
 
     /**
-     * ToDo: fix
      * {@inheritdoc}
      */
     public function create(array $options = []): BaseShippingMethodInterface
@@ -91,7 +53,6 @@ class ShippingMethodFactory extends ShippingMethodExampleFactory
     }
 
     /**
-     * ToDo: fix
      * {@inheritdoc}
      */
     protected function configureOptions(OptionsResolver $resolver): void
