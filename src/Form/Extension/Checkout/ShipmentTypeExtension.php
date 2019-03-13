a844704312aa9f1da8cde5ad304e2d75abb0a0cc
Danijela Mikulicic <danijela2405@gmail.com>
2019-03-13 15:19:24 +0100

    added shipment validation on correct step of checkout form

--- src/Form/Extension/Checkout/ShipmentTypeExtension.php
+++ src/Form/Extension/Checkout/ShipmentTypeExtension.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Locastic\SyliusStoreLocatorPlugin\Form\Extension\Checkout;
 
 use Locastic\SyliusStoreLocatorPlugin\Entity\Store;
+use Locastic\SyliusStoreLocatorPlugin\Form\EventListener\ShipmentFormEventListener;
 use Sylius\Bundle\CoreBundle\Form\Type\Checkout\ShipmentType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractTypeExtension;
@@ -13,6 +14,13 @@ use Doctrine\ORM\EntityRepository;
 
 final class ShipmentTypeExtension extends AbstractTypeExtension
 {
+    private $translator;
+
+    public function __construct($translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add(
@@ -25,10 +33,14 @@ final class ShipmentTypeExtension extends AbstractTypeExtension
                         ->setParameter('pickupAtStoreAvailable', true);
                 },
                 'mapped' => true,
+                'required' => false,
                 'class' => Store::class,
                 'choice_label' => 'name',
             ]
         );
+
+        $message = $this->translator->trans('locastic_sylius_store_locator_plugin.shipment_method.store_not_null',[],'validators');
+        $builder->addEventSubscriber(new ShipmentFormEventListener($message));
     }
 
     public static function getExtendedTypes(): iterable
